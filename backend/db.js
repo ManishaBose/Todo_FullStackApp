0c4292d19312bfb306a976639a7f667cba397fd7
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-09 00:23:49 +0530

    Add mongodb database schema

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI);
+
+const todoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+const Todo = mongoose.model('Todo', todoSchema);
+module.exports={
+    Todo
+}
