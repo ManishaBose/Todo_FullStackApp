0bc67fb5fd324d1e6bf5a3afb1ec6dd549cd76ec
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-09 15:01:32 +0530

    Implement CreateTodo.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import { CreateTodo } from './components/CreateTodo'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
       <div>
-        Hello World
+        <CreateTodo/>
       </div>
   )
 }
