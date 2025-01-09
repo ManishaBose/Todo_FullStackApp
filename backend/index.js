3d913dce3532515327e87ab3059ddff53a8ecbe6
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-09 11:19:48 +0530

    Implement GET, POST, and PUT routes logic

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { createTodoSchema, updateTodoSchema } = require('./types');
+const { Todo } = require('./db');
 const PORT = 3000;
 const app = express();
 
@@ -15,24 +16,63 @@ function validatePutInput(obj){
     return response;
 }
 
-app.get("/",(req,res)=>{
-    //res.send("Hello World!");
+app.get("/",async (req,res)=>{
+    const todos = await Todo.find({});
+    res.status(200).json({
+        todos
+    })
 })
 
-app.post("/",(req,res)=>{
+app.post("/",async (req,res)=>{
     const validation = validatePostInput(req.body);
     if(!validation.success){
         return res.status(400).json({
             message: "Invalid input. Try again"
         })
     }
+    //validation.data
+    const {title, description} = validation.data;
+    try{
+        await Todo.create({
+            title,
+            description,
+            completed: false
+        });
+        return res.status(200).json({
+            message: "Todo added."
+        })
+    } catch(e){
+        console.error(e);
+        res.status(500).json({
+            message: "Todo couldn't be created."
+        })
+    }
 });
 
-app.put("/",(req,res)=>{
+app.put("/",async (req,res)=>{
     const validation = validatePutInput(req.body);
     if(!validation.success){
         return res.status(400).json({
             message: "Invalid input. Try again"
+        })
+    }
+    const id = validation.data.id;
+    try{
+        const result = await Todo.updateOne({_id : id}, {$set: {
+            completed: true
+        }});
+        if(result.matchedCount === 0){
+            return res.status(404).json({
+                message: "Todo not found"
+            })
+        }
+        return res.status(200).json({
+            message: "Todo completed."
+        })
+    } catch(e){
+        console.error(e);
+        return res.status(500).json({
+            message: "Internal server error"
         })
     }
 })
