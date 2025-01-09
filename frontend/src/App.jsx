9fdcccfbbc42afd4492c6456bf90d10656b7b8b5
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-10 01:17:28 +0530

    Implement Todos.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
+import { Todos } from './components/Todos'
 
 function App() {
-  
+  const todoTemp = [{
+      "_id": {
+        "$oid": "677f61dd2c4c6219dc2f697b"
+      },
+      "title": "Finish building Todo App",
+      "description": "Use React for frontend, node for backend, and mongodb for database",
+      "completed": true,
+      "__v": 0
+  },{
+    "_id": {
+      "$oid": "6780151eda945e2fd33efd04"
+    },
+    "title": "LeetCode Daily Problem",
+    "description": "A question on string matching",
+    "completed": false,
+    "__v": 0
+  }]
   return (
       <div>
         <CreateTodo/>
+        <Todos todos={todoTemp}/>
       </div>
   )
 }
---
+++ frontend/src/components/Todos.jsx
@@ -0,0 +1,12 @@
+export function Todos({todos}){
+    return (
+        <div>
+            {todos.map((todo)=>{
+            return <div>
+                <h1>{todo.title}</h1>
+                <p>{todo.description}</p>
+                <button>{todo.completed==true ? "Completed":"Mark as complete"}</button>
+            </div>
+            })}
+    </div>)
+}
