cc00c0c30751f1686612bc2a2083b4a5d4007b05
MANISHA BOSE <manishabose.2001@gmail.com>
2025-01-11 18:15:24 +0530

    Implement update todos logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -5,7 +5,22 @@ export function Todos({todos}){
             return <div>
                 <h1>{todo.title}</h1>
                 <p>{todo.description}</p>
-                <button>{todo.completed==true ? "Completed":"Mark as complete"}</button>
+                <button onClick={()=>{
+                    fetch("http://localhost:3000/",
+                    {
+                        method: "PUT",
+                        body: JSON.stringify({
+                            id: todo._id,
+                        }),
+                        headers: {
+                            "Content-type": "application/json"
+                        }
+                    })
+                        .then(async function(res){
+                            const json = await res.json();
+                            alert("Todo completed!")
+                        })
+                }}>{todo.completed==true ? "Completed":"Mark as complete"}</button>
             </div>
             })}
     </div>)
